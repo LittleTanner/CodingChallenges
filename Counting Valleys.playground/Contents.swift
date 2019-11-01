99b30fbcc1ddad2a831f7e6775a815c7208c0e22
Kevin Tanner <kevindavidtanner@gmail.com>
2019-11-01 15:07:09 -0700

    Solved

---
+++ Counting Valleys.playground/Contents.swift
@@ -0,0 +1,79 @@
+import UIKit
+
+/*
+ 
+ Gary is an avid hiker. He tracks his hikes meticulously, paying close attention to small details like topography. During his last hike he took exactly  steps. For every step he took, he noted if it was an uphill, , or a downhill,  step. Gary's hikes start and end at sea level and each step up or down represents a  unit change in altitude. We define the following terms:
+ A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+ A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+ Given Gary's sequence of up and down steps during his last hike, find and print the number of valleys he walked through.
+ For example, if Gary's path is , he first enters a valley  units deep. Then he climbs out an up onto a mountain  units high. Finally, he returns to sea level and ends his hike.
+ Function Description
+ Complete the countingValleys function in the editor below. It must return an integer that denotes the number of valleys Gary traversed.
+ countingValleys has the following parameter(s):
+ n: the number of steps Gary takes
+ s: a string describing his path
+ Input Format
+ The first line contains an integer , the number of steps in Gary's hike.
+ The second line contains a single string , of  characters that describe his path.
+ Constraints
+
+
+ Output Format
+ Print a single integer that denotes the number of valleys Gary walked through during his hike.
+ Sample Input
+ 8
+ UDDDUDUU
+ Sample Output
+ 1
+ Explanation
+ If we represent _ as sea level, a step up as /, and a step down as \, Gary's hike can be drawn as:
+ _/\      _
+    \    /
+     \/\/
+ He enters and leaves one valley.
+
+ */
+
+func countingValleys(n: Int, s: String) -> Int {
+    var numberOfValleys = 0
+    var arrayOfSteps: [Int] = []
+    
+    var stepsUp = 0
+    
+    for character in s {
+        if character == "U" {
+            stepsUp += 1
+        }
+        if character == "D" {
+            stepsUp -= 1
+        }
+        arrayOfSteps.append(stepsUp)
+    }
+
+    var inAValley = false
+    
+    if arrayOfSteps[0] < 0 {
+        inAValley = true
+        numberOfValleys += 1
+    } else {
+        inAValley = false
+    }
+    
+    for step in arrayOfSteps {
+        if inAValley == false && step < 0 {
+            inAValley = true
+            numberOfValleys += 1
+        }
+        
+        if inAValley == true && step >= 0 {
+            inAValley = false
+        }
+    }
+    
+    return numberOfValleys
+}
+
+countingValleys(n: 8, s: "UDDDUDUU") // Should = 1
+countingValleys(n: 8, s: "UUUUDDDD") // Should = 0
+countingValleys(n: 12, s: "DDUUDDUDUUUD") // Should = 2
+countingValleys(n: 100, s: "DDUUDDDUDUUDUDDDUUDDUDDDUDDDUDUUDDUUDDDUDDDUDDDUUUDUDDDUDUDUDUUDDUDUDUDUDUUUUDDUDDUUDUUDUUDUUUUUUUUU") // Should = 2
