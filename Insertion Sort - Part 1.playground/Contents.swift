dfda83684bc3600a73aec4ff0f74e94c77c32749
Kevin Tanner <kevindavidtanner@gmail.com>
2019-11-14 16:06:17 -0800

    Solved

---
+++ Insertion Sort - Part 1.playground/Contents.swift
@@ -0,0 +1,84 @@
+import UIKit
+
+/*
+ 
+ Sorting
+ One common task for computers is to sort data. For example, people might want to see all their files on a computer sorted by size. Since sorting is a simple problem with many different possible solutions, it is often used to introduce the study of algorithms.
+ Insertion Sort
+ These challenges will cover Insertion Sort, a simple and intuitive sorting algorithm. We will first start with a nearly sorted list.
+ Insert element into sorted list
+ Given a sorted list with an unsorted number  in the rightmost cell, can you write some simple code to insert  into the array so that it remains sorted?
+ Since this is a learning exercise, it won't be the most efficient way of performing the insertion. It will instead demonstrate the brute-force method in detail.
+ Assume you are given the array  indexed . Store the value of . Now test lower index values successively from  to  until you reach a value that is lower than ,  in this case. Each time your test fails, copy the value at the lower index to the current index and print your array. When the next lower indexed value is smaller than , insert the stored value at the current index and print the entire array.
+ The results of operations on the example array is:
+ Starting array:
+ Store the value of  Do the tests and print interim results:
+ 1 2 4 5 5
+ 1 2 4 4 5
+ 1 2 3 4 5
+ Function Description
+ Complete the insertionSort1 function in the editor below. It should print the interim and final arrays, each on a new line.
+ insertionSort1 has the following parameter(s):
+ n: an integer, the size of
+ arr: an array of integers to sort
+ Input Format
+ The first line contains the integer , the size of the array .
+ The next line contains  space-separated integers .
+ Constraints
+  
+
+ Output Format
+ Print the array as a row of space-separated integers each time there is a shift or insertion.
+ Sample Input
+ 5
+ 2 4 6 8 3
+ Sample Output
+ 2 4 6 8 8
+ 2 4 6 6 8
+ 2 4 4 6 8
+ 2 3 4 6 8
+ Explanation
+  is removed from the end of the array.
+ In the st line , so  is shifted one cell to the right.
+ In the nd line , so  is shifted one cell to the right.
+ In the rd line , so  is shifted one cell to the right.
+ In the th line , so  is placed at position .
+ 
+ */
+
+
+func insertionSort1(n: Int, arr: [Int]) -> Void {
+    var mutableArray = arr
+    let valueToStore = arr[n - 1]
+    var i = n - 1
+    var stringOfInts = ""
+    
+    while i >= 0 {
+        if i - 1 >= 0 && mutableArray[i - 1] > valueToStore {
+            mutableArray[i] = mutableArray[i - 1]
+            i -= 1
+
+            stringOfInts = ""
+            for number in 0...(n - 2) {
+                stringOfInts.append("\(mutableArray[number]) ")
+            }
+            stringOfInts.append(String(mutableArray[n - 1]))
+            
+            print(stringOfInts)
+        } else {
+            mutableArray[i] = valueToStore
+            
+            stringOfInts = ""
+            for number in 0...(n - 2) {
+                stringOfInts.append("\(mutableArray[number]) ")
+            }
+            stringOfInts.append(String(mutableArray[n - 1]))
+            print(stringOfInts)
+            break
+        }
+    }
+}
+
+//insertionSort1(n: 5, arr: [1,2,4,5,3])
+insertionSort1(n: 5, arr: [2,4,6,8,3])
+insertionSort1(n: 10, arr: [2,3,4,5,6,7,8,9,10,1])
