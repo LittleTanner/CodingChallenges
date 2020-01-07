0e6fc08661e2561a2e02b40b9e870bbcab93f4bc
Kevin Tanner <kevindavidtanner@gmail.com>
2020-01-06 16:43:07 -0800

    Solved, updated solution for 1

--- Challenge20.playground/Contents.swift
+++ Challenge20.playground/Contents.swift
@@ -3,7 +3,11 @@ import XCTest
 
 func isPrime(number: Int) -> Bool {
 
-    if number > 1 && number != 2 && number % 2 == 0 {
+    if number == 1 {
+        return false
+    }
+    
+    if number > 1 && number % 2 == 0 {
         return false
     }
     
@@ -16,3 +20,5 @@ assert(isPrime(number: 13) == true, "Test failed")
 assert(isPrime(number: 4) == false, "Test failed")
 assert(isPrime(number: 9) == true, "Test failed")
 assert(isPrime(number: 16777259) == true, "Test failed")
+
+isPrime(number: 1)
