24464cbd2e1d9be75bdec57e3d1f8a32d5206202
Kevin Tanner <kevindavidtanner@gmail.com>
2020-01-06 16:37:14 -0800

    Solved

---
+++ Challenge20.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+import XCTest
+
+func isPrime(number: Int) -> Bool {
+
+    if number > 1 && number != 2 && number % 2 == 0 {
+        return false
+    }
+    
+    return true
+}
+
+// Tests
+assert(isPrime(number: 11) == true, "Test failed")
+assert(isPrime(number: 13) == true, "Test failed")
+assert(isPrime(number: 4) == false, "Test failed")
+assert(isPrime(number: 9) == true, "Test failed")
+assert(isPrime(number: 16777259) == true, "Test failed")
